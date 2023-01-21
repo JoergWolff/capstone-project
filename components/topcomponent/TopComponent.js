35e58f1bed97c053e1185b022c82610c0a7d3341
Joerg Wolff <neuerfisch@wolffclan.de>
2023-01-21 16:38:18 +0100

    modify TopComponent to show books only if bookcounter true

--- components/topcomponent/TopComponent.js
+++ components/topcomponent/TopComponent.js
@@ -2,10 +2,13 @@ import styled from "styled-components";
 
 export default function TopComponent({ bookcounter }) {
   return (
-    <StyledHead>
-      <h1>MEDIA DB</h1>
-      <span>{bookcounter}</span>
-    </StyledHead>
+    <>
+      <StyledHead>
+        <h1>MEDIA DB</h1>
+        {bookcounter ? <span>{bookcounter} books</span> : ``}
+      </StyledHead>
+      <hr />
+    </>
   );
 }
 
@@ -13,4 +16,5 @@ const StyledHead = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  padding: 10px;
 `;
