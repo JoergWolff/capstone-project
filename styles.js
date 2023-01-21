37c6a08a51f99677737bcbc3e28e658cd44988aa
Joerg Wolff <neuerfisch@wolffclan.de>
2023-01-21 16:10:11 +0100

    add global varibles to styles.js

--- styles.js
+++ styles.js
@@ -1,14 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+  :root {
+    --main-background-color: ivory;
+    --main-color: brown;
+  }
+  
   *,
   *::before,
   *::after {
     box-sizing: border-box;
+    padding: 0;
+    margin: 0;
   }
 
   body {
-    margin: 0;
     font-family: system-ui;
+    background-color: var(--main-background-color);
+    color: var(--main-color);
   }
 `;
