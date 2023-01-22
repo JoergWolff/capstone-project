1c93e28f2110a8141685c30cf0ac3ff56f73c21e
Joerg Wolff <neuerfisch@wolffclan.de>
2023-01-22 18:10:54 +0100

    change data.js

--- helpers/data/data.js
+++ helpers/data/data.js
@@ -6,7 +6,7 @@ export const initialData = [
     author: "Stephen King",
     title: "Feuerkind",
     subtitle: "",
-    coverImg: "",
+    coverImg: "https://covers.openlibrary.org/b/id/10137891-L.jpg",
     isbn10: "3-453-43273-8",
     teaser:
       "Die Hauptfigur in Feuerkind ist Charlene „Charlie“ McGee, ein junges Mädchen mit der Fähigkeit zur Pyrokinese: Sie kann mit ihrem Willen Feuer entfachen und beliebige Gegenstände in Brand setzen. Charlie wurde mit dem pyrokinetischen Talent geboren, nachdem ihre Eltern als Collegestudenten an einem Drogenexperiment teilnahmen. Anfangs wird ihre Fähigkeit durch die rigide Sauberkeitserziehung der Eltern unterbunden und durch ihre noch nicht vollständig entwickelte Hypophyse. Nur gelegentlich trat ihre Kraft hervor, z. B. im Krabbelalter, als sie über einen Teddy stolperte und diesen voller Wut verbrannte. Als Resultat des Drogenexperiments entwickelte Charlies Vater Andy die Fähigkeit, den Willen anderer Menschen mittels seiner Gedanken zu beeinflussen. Ihre Mutter verfügt über leichte Telekinese- und Telepathie-Fähigkeiten. Im Vergleich zu Charlies Fähigkeiten sind die Kräfte der Eltern jedoch schwach ausgeprägt.",
@@ -18,7 +18,7 @@ export const initialData = [
     author: "Stephen King",
     title: "Shinning",
     subtitle: "",
-    coverImg: "",
+    coverImg: "https://covers.openlibrary.org/b/id/1000182-L.jpg",
     isbn10: "3-404-13008-1",
     teaser:
       "Jack Torrance, ein ehemaliger Lehrer und „trockener“ Alkoholiker, hofft, sein Leben wieder in den Griff zu bekommen, indem er für einen Winter die Stelle des Hausmeisters im abgelegenen Overlook-Hotel annimmt. Jack hat seine Stelle als Englischlehrer verloren, weil er einen Schüler geschlagen hatte. Seinem Sohn Danny, den er innig liebt, brach er betrunken sogar den Arm, als dieser drei Jahre alt war und einige Papiere in Unordnung gebracht hatte. Jacks Frau Wendy erwägt, sich wegen dieser Wutanfälle scheiden zu lassen. Sie vertagt aber die Entscheidung und hofft, dass der Aufenthalt im Overlook-Hotel die drei wieder als Familie zusammenschweißen wird.",
@@ -30,7 +30,7 @@ export const initialData = [
     author: "Stephen King",
     title: "Carrie",
     subtitle: "",
-    coverImg: "",
+    coverImg: "https://covers.openlibrary.org/b/id/9256029-L.jpg",
     isbn10: "3-404-13121-5",
     teaser:
       "Im Mittelpunkt der Handlung steht die 16-jährige Schülerin Carietta White, genannt Carrie, die in der US-Kleinstadt Chamberlain lebt. Carrie wächst ohne ihren Vater auf, der vor ihrer Geburt auf einer Baustelle ums Leben gekommen ist. Bereits im Alter von drei Jahren lässt sie mit ihren telekinetischen Kräften einen Steinregen über das Haus niedergehen, in dem sie mit ihrer Mutter lebt, als diese ihr in religiösem Wahn die Augen herausschneiden will.Im Alter von 16 Jahren bekommt Carrie nach dem Sportunterricht unter der Dusche ihre erste Periode. Sie erschrickt und ihre Mitschülerinnen hänseln und demütigen sie deshalb. In der Folge treten einige Fälle von Telekinese als Reaktion auf Beleidigungen, Demütigungen oder Ignoranz ihrer Mitmenschen auf und Carrie wird sich der Tatsache bewusst, dass diese mysteriösen Ereignisse von ihr verursacht worden sind. Sie denkt neu über die Beziehung zu ihrer Mutter nach, die alles Körperliche und Sexuelle als grässliche Sünde darstellt und über Jahre verhindert hat, dass Carrie wie ein normales Mädchen aufwachsen konnte. Nachdem ihre Mutter sie vorher häufig hart bestraft hat, gelingt es Carrie, die sich nun ihrer Macht bewusst wird, den Einfluss ihrer Mutter zurückzudrängen.",
@@ -42,7 +42,7 @@ export const initialData = [
     author: "Stephen King",
     title: "Der dunkle Turm",
     subtitle: "Schwarz",
-    coverImg: "",
+    coverImg: "https://covers.openlibrary.org/b/id/9324956-L.jpg",
     isbn10: "3-453-87556-7",
     teaser:
       "Roland von Gilead, letzter Revolvermann einer uralten Gilde von ritterähnlichen Gesetzesmännern aus der Linie des Eld, des ersten Revolvermanns, hat seine Jugend und seine durch Verrat und Verfall zerstörte Heimat hinter sich gelassen und ist auf der Suche nach dem dunklen Turm. Dieser Turm ist ein Brennpunkt aller Energie des Universums zwischen den Welten, und seit die Welt sich weiterbewegt hat, scheinen die Dinge dort falsch zu laufen.",
@@ -54,7 +54,7 @@ export const initialData = [
     author: "Stephen King",
     title: "Der dunkle Turm",
     subtitle: "Drei",
-    coverImg: "",
+    coverImg: "https://covers.openlibrary.org/b/id/1008826-L.jpg",
     isbn10: "3-453-87557-5",
     teaser:
       "Drei führt die Geschichte von Roland und seiner Suche nach dem Dunklen Turm nahtlos fort. Nachdem der erste Teil der Saga mit einem zehnjährigen Palaver („Unterhaltung“ in der Sprache des Revolvermanns) mit dem „Mann in Schwarz“ endete, findet sich der Revolvermann in Drei am Strand des Westlichen Meeres wieder. Hier kriechen in der Abenddämmerung todbringende Monster-Hummer aus dem Meer und verletzen Roland schwer. Der Revolvermann überlebt ihre Attacke, verliert dabei aber zwei Finger, einen Zeh und erleidet eine Blutvergiftung. Dennoch gelingt es ihm schwer erkrankt, die erste von drei Türen zu finden, die ihm der „Mann in Schwarz“ prophezeit hat.",
@@ -66,7 +66,7 @@ export const initialData = [
     author: "Stephen King",
     title: "Der dunkle Turm",
     subtitle: "Tot",
-    coverImg: "",
+    coverImg: "https://covers.openlibrary.org/b/id/10515631-L.jpg",
     isbn10: "3-453-87558-3",
     teaser:
       "Am Anfang von tot. verlassen Roland, Eddie, ein ehemaliger Junkie aus New York und Susannah, eine Symbiose der beiden Charaktere aus der schizophrenen Odetta, das Westliche Meer und betreten einen großen, alten Wald. Roland lehrt Eddie und Susannah die Grundregeln des Revolvermanntums. Beide lernen schnell und das müssen sie auch, denn sie werden von Shardik, einem uralten, gigantischen Cyborg-Bären angegriffen, der vom Alten Volk damit beauftragt wurde, ein Portal des Balkens zu beschützen. Alles in allem gibt es zwölf solcher Portale in Rolands Welt, die alle von einem anderen Tier bewacht werden. Diese Portale sind zugleich Anfangs- und Endpunkt von sechs Balken, die fast unsichtbar, aber trotzdem mächtiger als vieles andere auf der Welt, in ihrer Mitte zusammenlaufen und dort den Dunklen Turm stützen. Sie besiegen Shardik schließlich, indem sie die Antenne auf dessen riesigen Kopf abschießen und folgen dem Pfad des Balkens in Richtung des dunklen Turmes. Roland hat in der dritten Tür in „Drei“ den Mann getötet, der den jungen Jake vor ein Auto stoßen sollte, bevor er selbst ihn genau davon abgehalten hat.",
@@ -78,7 +78,7 @@ export const initialData = [
     author: "Stephen King",
     title: "Der dunkle Turm",
     subtitle: "Glas",
-    coverImg: "",
+    coverImg: "https://covers.openlibrary.org/b/id/1009068-L.jpg",
     isbn10: "3-453-87559-1",
     teaser:
       "Das Buch beginnt mit der Fortsetzung des Rätselwettbewerbs. Dieser endet damit, dass Eddie sich den Kopf zerbricht und letztendlich Blaine den Mono mit Unlogik besiegt (Wie kam das tote Baby überhaupt auf die Straße? Weil es auf dem Huhn festgetackert war.). Der Revolvermann, welcher zuvor die „blöden“ Rätsel verschmäht hatte, muss nun anerkennen, dass selbst Unlogik manchmal weiterhilft. Die Reise endet damit, dass der schizophrenen Intelligenz namens Blaine die dipolaren Schaltkreise durchbrennen, was Eddie mit mehreren Schüssen in den Computer von Blaine noch beschleunigt, und der Zug so noch rechtzeitig zum Stillstand kommt.",
