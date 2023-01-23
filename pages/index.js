0eb3d19d6dff2473e4e02c1e4e135058ed5318c7
Joerg Wolff <neuerfisch@wolffclan.de>
2023-01-23 15:57:11 +0100

    change import to prop data

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,18 @@
 import Card from "@/components/Card/Card";
-import { initialData } from "@/helpers/data/data";
 
-export default function HomePage() {
-  const data = initialData;
+export default function HomePage({ data }) {
   return (
-    <>
-      <ul>
-        {data.map((book) => (
-          <li key={book.id}>
-            <Card
-              coverimage={book.coverImg}
-              author={book.author}
-              title={book.title}
-              subtitle={book.subtitle}
-            />
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul>
+      {data.map((book) => (
+        <li key={book.id}>
+          <Card
+            coverimage={book.coverImg}
+            author={book.author}
+            title={book.title}
+            subtitle={book.subtitle}
+          />
+        </li>
+      ))}
+    </ul>
   );
 }
