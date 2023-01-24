4072f5598efed6a5dfa140e63345da371e6ef56e
Joerg Wolff <neuerfisch@wolffclan.de>
2023-01-24 16:20:20 +0100

    create teaser at detail page

--- pages/detail/book/[id].js
+++ pages/detail/book/[id].js
@@ -1,4 +1,6 @@
+import styled from "styled-components";
 import { useRouter } from "next/router";
+import Card from "@/components/Card/Card";
 
 export default function DetailBookPage({ data }) {
   const router = useRouter();
@@ -9,5 +11,31 @@ export default function DetailBookPage({ data }) {
   if (!currentBook) {
     return <h1>404 An error occured...</h1>;
   }
-  return <h1>{currentBook.title}</h1>;
+  return (
+    <>
+      <Card book={currentBook} />
+      <StyledCard>
+        <StyledId>
+          <span>ID:</span>
+          <span>{currentBook.id}</span>
+        </StyledId>
+        <h4>Teaser:</h4>
+        <article>{currentBook.teaser}</article>
+      </StyledCard>
+    </>
+  );
 }
+
+const StyledCard = styled.div`
+  margin: var(--main-margin);
+  background-color: var(--main-card-background-color);
+  border: var(--main-card-border);
+  border-radius: var(--main-card-border-radius);
+  padding: var(--main-card-padding);
+`;
+
+const StyledId = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 10px;
+`;
