3016cfc7ffb0a0cc2a4125eed1b44f402098a9fd
Joerg Wolff <neuerfisch@wolffclan.de>
2023-01-26 17:14:15 +0100

    create first BookForm draft

--- components/BookForm/BookForm.js
+++ components/BookForm/BookForm.js
@@ -1,3 +1,68 @@
+import Image from "next/image";
+import styled from "styled-components";
+import dummyBook from "../../public/img/dummy/dummy_book.png";
+
 export default function BookForm({ book }) {
-  return <h1>BookForm</h1>;
+  return (
+    <StyledForm>
+      <StyledTopForm>
+        {book.coverImg ? (
+          <Image
+            src={book.coverImg}
+            width={70}
+            height={100}
+            alt={book.title}
+            priority
+          />
+        ) : (
+          <Image
+            src={dummyBook}
+            width={70}
+            height={100}
+            alt="No picture"
+            priority
+          />
+        )}
+        <StyledUrl>
+          <label htmlFor="url-input">URL:</label>
+          <textarea
+            defaultValue={book?.coverImg}
+            type="text"
+            id="url-input"
+            rows="5"
+            cols="33"
+          />
+        </StyledUrl>
+      </StyledTopForm>
+
+      <label htmlFor="author-input">Author:</label>
+      <input defaultValue={book?.author} type="text" id="author-input" />
+
+      <label htmlFor="title-input">Title:</label>
+      <input defaultValue={book?.title} type="text" id="title-input" />
+
+      <label htmlFor="subtitle-input">Subtitle:</label>
+      <input defaultValue={book?.subtitle} type="text" id="subtitle-input" />
+
+      <label htmlFor="teaser-input">Teaser:</label>
+      <textarea defaultValue={book?.teaser} id="teaser-input" rows="20" />
+    </StyledForm>
+  );
 }
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+`;
+
+const StyledTopForm = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const StyledUrl = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 10px;
+`;
