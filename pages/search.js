d6d739fbb540c581337d2078d73822c0d2840a81
Joerg Wolff <neuerfisch@wolffclan.de>
2023-02-19 13:14:10 +0100

    create first search page draft

---
+++ pages/search.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+import StyledContent from "@/components/StyledContent/StyledContent";
+import StyledNavigation from "@/components/StyledNavigation/StyledNavigation";
+
+export default function SearchBookPage() {
+  return (
+    <>
+      <StyledContent>
+        <h1>SearchBookPage</h1>
+      </StyledContent>
+      <StyledNavigation>
+        <StyledLinkPlaceholder />
+        <StyledLink href="/">Home</StyledLink>
+        <StyledLinkPlaceholder />
+      </StyledNavigation>
+    </>
+  );
+}
+
+const StyledLink = styled(Link)`
+  border: var(--main-link-button-border);
+  border-radius: var(--main-link-button-border-radius);
+  padding: var(--main-link-button-padding);
+  width: var(--main-link-button-width);
+  color: var(--main-link-button-color);
+  text-align: center;
+  text-decoration: none;
+`;
+
+const StyledLinkPlaceholder = styled.div`
+  width: var(--main-link-button-width);
+`;
